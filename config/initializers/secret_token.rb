05369945b7451fbd177494db7cbdc38da2230389
gogojimmy <jimmy@gogojimmy.net>
2012-08-09 23:55:48 +0800

    First commit with devise gem bundled

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BaseWithDevise::Application.config.secret_token = '5007c6f1fba61325b6d3acb427bb31647f95848b33841e27948bd7eb974e4d0b270759f024029de6e7f93d1809bc8439dc79b6cf953a9856e1321ff77bbc0d21'
