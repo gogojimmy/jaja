74208884c899d40fc741c9644dce3245c54d2324
gogojimmy <jimmy@gogojimmy.net>
2012-09-15 20:10:42 +0800

    Deploy config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :find_projects
+  before_filter :authenticate
 
   protected
 
   def find_projects
     @all_projects = Project.published
   end
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+       username == "jimmy" && password == "0975519929"
+     end
+  end
+
 end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :rails_env, "production"
-set :user, "deployer"
-set :domain, "example.com"
+set :user, "jaja"
+set :domain, "jaja.gogojimmy.net"
 set :branch, "master"
 
 server "#{domain}", :web, :app, :db, :primary => true
