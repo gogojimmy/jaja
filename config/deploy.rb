fe116d968a20f6785e96569ac974b22048beceb4
Jimmy Kuo <jimmy@gogojimmy.net>
2012-09-21 08:59:08 +0800

    Deploy for heroku

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,18 +44,18 @@ namespace :deploy do
     #task :precompile, :role => :app do
       #run "cd #{release_path}/ && RAILS_ENV=staging bundle exec rake assets:precompile --trace"
     #end
-    task :precompile, :roles => :web, :except => { :no_release => true } do
-      from = source.next_revision(current_revision)
-      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-        run_locally "bundle exec rake assets:precompile"
-        find_servers_for_task(current_task).each do |server|
-          run_locally "rsync -vr --exclude='.DS_Store' public/assets #{user}@#{server.host}:#{shared_path}/"
-        end
-        run_locally "rm -rf public/assets/*"
-      else
-        logger.info "Skipping asset pre-compilation because there were no asset changes"
-      end
-    end
+    #task :precompile, :roles => :web, :except => { :no_release => true } do
+      #from = source.next_revision(current_revision)
+      #if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        #run_locally "bundle exec rake assets:precompile"
+        #find_servers_for_task(current_task).each do |server|
+          #run_locally "rsync -vr --exclude='.DS_Store' public/assets #{user}@#{server.host}:#{shared_path}/"
+        #end
+        #run_locally "rm -rf public/assets/*"
+      #else
+        #logger.info "Skipping asset pre-compilation because there were no asset changes"
+      #end
+    #end
   end
 end
 
